d5b354d4659d38ea10c4a2554c4c90811f4b28ee
flipdeciantis <111934683+flipdeciantis@users.noreply.github.com>
2022-12-08 11:56:50 -0500

    Adding in web app structure

---
+++ app/project.py
@@ -0,0 +1,62 @@
+#code to collect GDP and Retail Sales data
+
+import requests
+import json
+from IPython.display import Image, display 
+
+
+def display_feed(symbol="NFLX"):
+    API_KEY = input("Please input your ALPHAVANTAGE API KEY: ")
+    request_url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={symbol}&apikey={API_KEY}"
+    
+    response = requests.get(request_url)
+
+    data = json.loads(response.text)
+    #print(type(data)) #> dict
+    #print(data.keys())
+    if "feed" not in data.keys():
+        print(data)
+        return None
+
+API_KEY = input("Please input your ALPHAVANTAGE API KEY: ")
+
+def fetch_GDP_data():
+    """Fetches GDP data from the AlphaVantage API. Returns a list of dictionaries."""
+    
+    request_url = f"https://www.alphavantage.co/query?function=REAL_GDP&apikey={API_KEY}"
+
+    response = requests.get(request_url)
+
+    parsed_response = json.loads(response.text)
+    
+    return parsed_response["data"]
+
+def fetch_RS_data():
+    """Fetches GDP data from the AlphaVantage API. Returns a list of dictionaries."""
+    
+    request_url = f"https://www.alphavantage.co/query?function=RETAIL_SALES&apikey={API_KEY}"
+
+    response = requests.get(request_url)
+
+    parsed_response = json.loads(response.text)
+    
+    return parsed_response["data"]
+
+
+if __name__ == "__main__":
+
+    print("GDP REPORT...")
+
+    data = fetch_GDP_data()
+
+    print("-------------------------")
+    print("LATEST GDP RATE:")
+    print(f"{data[0]['value']}", "as of", data[0]["date"])
+
+    print("Retail Sales REPORT...")
+
+    data = fetch_RS_data()
+
+    print("-------------------------")
+    print("LATEST Retail Sales RATE:")
+    print(f"{data[0]['value']}", "as of", data[0]["date"])
---
+++ web_app/__init__.py
@@ -0,0 +1,29 @@
+# this is the "web_app/__init__.py" file...
+
+#import os
+#from dotenv import load_dotenv
+from flask import Flask
+
+from web_app.routes.project_routes import project_routes
+from web_app.routes.stocks_routes import stocks_routes
+#from web_app.routes.book_routes import book_routes
+#from web_app.routes.weather_routes import weather_routes
+
+#load_dotenv()
+
+#SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+
+
+def create_app():
+    app = Flask(__name__)
+    #app.config["SECRET_KEY"] = SECRET_KEY
+
+    app.register_blueprint(project_routes)
+    app.register_blueprint(stocks_routes)
+    #app.register_blueprint(book_routes)
+    #app.register_blueprint(weather_routes)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
