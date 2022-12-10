c7b4c8550dfbf60c801000082a9d325b0ee8edb4
flipdeciantis <111934683+flipdeciantis@users.noreply.github.com>
2022-12-09 21:23:14 -0500

    updated testing

--- test/project_test.py
+++ test/project_test.py
@@ -1,5 +1,6 @@
 # this is the "test/project_test.py" file...
 
-from app.project import display_feed, fetch_RS_data, fetch_GDP_data
+#from app.project import display_feed, fetch_RS_data, fetch_GDP_data
 
-from pandas import DataFrame
+#from pandas import DataFrame
+
